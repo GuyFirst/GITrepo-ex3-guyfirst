fe104c28b1940a81b04f98ff4c51ced5b64828d1
Guy First <guyfirst19@gmail.com>
2026-04-16 14:54:27 +0300

    printing  something else

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
 	public static void main(String[] args) {
 		for (int i=1;i<=10; i++) {
-			System.out.println("THIS IS SPARTA AGAIN!!!!!!!!!!..."+i);
+			System.out.println("printing something else!!!!!!!!!!..."+i);
 		}
 	}
 }
