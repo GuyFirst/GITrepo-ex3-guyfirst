d730dafb9f7fa886e5d04d9d1295094eacb3f4d3
Guy First <guyfirst19@gmail.com>
2026-04-17 11:06:15 +0300

    added a line to print

--- Guyfirst.java
+++ Guyfirst.java
@@ -1,7 +1,7 @@
 public class Guyfirst {
 	public static void main(String[] args) {
 		for (int i=1;i<=10; i++) {
-			System.out.println("Guy First ..."+i);
+			System.out.println("this is my name ..."+i);
 		}
 	}
 }
--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello {
 	public static void main(String[] args) {
 		for (int i=1;i<=10; i++) {
 			System.out.println("printing something else!!!!!!!!!!..."+i);
+			System.out.println("this is cool..."+ 2*i);
 		}
 	}
 }
