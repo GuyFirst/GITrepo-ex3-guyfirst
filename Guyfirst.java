3c3df03c3d254cde49867a2832390f66191b4ba7
Guy First <guyfirst19@gmail.com>
2026-04-17 11:09:19 +0300

    last change

--- Guyfirst.java
+++ Guyfirst.java
@@ -2,6 +2,7 @@ public class Guyfirst {
 	public static void main(String[] args) {
 		for (int i=1;i<=10; i++) {
 			System.out.println("this is my name ..."+i);
+			System.out.println("Guy, First! ..."+ i*i);
 		}
 	}
 }
